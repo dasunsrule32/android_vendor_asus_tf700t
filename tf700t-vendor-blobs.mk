8518c84953c8f804a755d442b1f94b2176954139
Aaron Echols <atechols@gmail.com>
2012-10-22 18:11:29 -0700

    add tf_daemon blob entry
    Change-Id: Ib1bd4a5d16b8a86a06325ad60a14a8ff3869e086

--- tf700t-vendor-blobs.mk
+++ tf700t-vendor-blobs.mk
@@ -28,6 +28,7 @@ PRODUCT_COPY_FILES += \
     vendor/asus/tf700t/proprietary/bin/rild:system/bin/rild \
     vendor/asus/tf700t/proprietary/bin/rm_ts_server:system/bin/rm_ts_server \
     vendor/asus/tf700t/proprietary/bin/sensors-config:system/bin/sensors-config \
+    vendor/asus/tf700t/proprietary/bin/tf_daemon:system/bin/tf_daemon \
     vendor/asus/tf700t/proprietary/bin/touch_fw_update:system/bin/touch_fw_update \
     vendor/asus/tf700t/proprietary/bin/wifimacwriter:system/bin/wifimacwriter \
     vendor/asus/tf700t/proprietary/etc/asound.conf:system/etc/asound.conf \
